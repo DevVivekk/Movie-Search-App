248887a27f12e635e77733b3e6e850e21bc253d5
DevVivekk <vivekbhardwajvk2202@gmail.com>
2023-03-14 21:24:22 +0530

    api

--- src/movie.js
+++ src/movie.js
@@ -4,8 +4,7 @@ import { MovieCard } from './moviecard'
 const Movie = () => {
      const [values,setValues] = useState([])
     const findmovie = async(item)=>{
-        const api= `http://www.omdbapi.com/?i=tt3896198&apikey=d7cf4fbd&s=${item?.length>1?`${item}`:'hanuman'}`
-        const res = await fetch(api)
+        const res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=d7cf4fbd&s=${item?.length>1?`${item}`:'hanuman'}`)
         const data = await res.json();
         setValues(data.Search);
     }
